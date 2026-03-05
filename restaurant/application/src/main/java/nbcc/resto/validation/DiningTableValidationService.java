1b3e3ec8f2650be6eb012a604e0e67da8dcdbe95
mrwhatbrian <brianc92622@gmail.com>
2026-03-04 23:30:42 -0400

    refactor: DiningTable Service and Validation

--- restaurant/application/src/main/java/nbcc/resto/validation/DiningTableValidationService.java
+++ restaurant/application/src/main/java/nbcc/resto/validation/DiningTableValidationService.java
@@ -1,22 +1,34 @@
 package nbcc.resto.validation;
 
+import nbcc.common.service.AnnotationValidationService;
 import nbcc.common.validation.ValidationError;
 import nbcc.resto.dto.DiningTable;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 @Service
 public class DiningTableValidationService {
 
-    public Collection<ValidationError> validate(DiningTable diningTable) {
+    private final AnnotationValidationService annotationValidationService;
+
+    public DiningTableValidationService(AnnotationValidationService annotationValidationService) {
+        this.annotationValidationService = annotationValidationService;
+    }
+
+    public Collection<ValidationError> validate(DiningTable table) {
         var errors = new ArrayList<ValidationError>();
-
-        if (diningTable.getCapacity() <= 0) {
-            errors.add(new ValidationError("Capacity must be greater than 0", "capacity", diningTable.getCapacity()));
-        }
-
+        errors.addAll(annotationValidationService.validate(table));
+        errors.addAll(validateCapacity(table));
         return errors;
+    }
+
+    private Collection<ValidationError> validateCapacity(DiningTable table) {
+        if (table.getCapacity() <= 0) {
+            return List.of(new ValidationError("Capacity must be greater than 0", "capacity"));
+        }
+        return List.of();
     }
 }
