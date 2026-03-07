2e53561cedf1d809dee802c2c3f18b6c646c3d6a
Matheus Ferraro <matheus.ferraro@gmail.com>
2026-03-07 14:27:52 -0400

    feat: Add event details endpoint

--- restaurant/web/src/main/java/nbcc/resto/controller/EventController.java
+++ restaurant/web/src/main/java/nbcc/resto/controller/EventController.java
@@ -113,6 +113,19 @@ public class EventController {
         return "redirect:/events";
     }
 
+    @GetMapping("/details/{id}")
+    public String details(@PathVariable Long id, Model model){
+        var result = eventService.get(id);
+
+        if(result.isError() || result.isEmpty()){
+            model.addAttribute("message", "Event not found");
+            return "error/errorPage";
+        }
+
+        model.addAttribute("event", result.getValue());
+        return "event/details";
+    }
+
 
 
 
