34e1715b7632a971722e46deb81e728e96739a31
mrwhatbrian <brianc92622@gmail.com>
2026-03-17 01:50:47 -0300

    feat: Build Thymeleaf Templates (Update & Delete)

--- restaurant/web/src/main/java/nbcc/resto/viewmodels/MenuListViewModel.java
+++ restaurant/web/src/main/java/nbcc/resto/viewmodels/MenuListViewModel.java
@@ -9,12 +9,16 @@ public class MenuListViewModel {
     private final Collection<Menu> menus;
 
     private final boolean canAdd;
+    private final boolean canEdit;
+    private final boolean canDelete;
 
     private String message;
 
     public MenuListViewModel(Collection<Menu> menus, boolean canManage) {
         this.menus = menus;
         this.canAdd = canManage;
+        this.canEdit = canManage;
+        this.canDelete = canManage;
         this.message = message;
     }
 
@@ -24,6 +28,14 @@ public class MenuListViewModel {
 
     public boolean isCanAdd() {
         return canAdd;
+    }
+
+    public boolean isCanEdit() {
+        return canEdit;
+    }
+
+    public boolean isCanDelete() {
+        return canDelete;
     }
 
     public String getMessage() {
