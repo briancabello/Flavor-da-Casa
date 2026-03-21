8ffa1489a47453a4553397b7ceec033f0bfe479c
mrwhatbrian <brianc92622@gmail.com>
2026-03-21 03:44:47 -0300

    feat: Implement Menu Items Service and Validation Logic

---
+++ restaurant/application/src/main/java/nbcc/resto/service/MenuItemService.java
@@ -0,0 +1,16 @@
+package nbcc.resto.service;
+
+import nbcc.common.result.Result;
+import nbcc.common.result.ValidatedResult;
+import nbcc.resto.dto.MenuItem;
+
+import java.util.Collection;
+
+public interface MenuItemService {
+
+    Result<Collection<MenuItem>> getByMenuId(Long menuId);
+
+    ValidatedResult<MenuItem> get(Long id);
+
+    ValidatedResult<MenuItem> create(MenuItem menuItem);
+}
